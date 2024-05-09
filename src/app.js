65608ab59582bff00af7fbf5a51d23c958b8a0fc
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-05-09 18:30:58 +0000

    se creo un generador de cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generarCarta() {
+  let palos = [
+    "<i class='fas fa-heart fa-2xs' style='color: #f40101;'></i>",
+    "<i class='fa-solid fa-diamond fa-2xs' style='color: #f50000;'></i>",
+    "&clubs;",
+    "&spades;"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let paloAleatorio = palos[Math.floor(Math.random() * palos.length)];
+
+  let valorAleatorio = valores[Math.floor(Math.random() * valores.length)];
+
+  return { valor: valorAleatorio, palo: paloAleatorio };
+}
+
+let cartaElemento = document.getElementById("carta");
+
+let cartaGenerada = generarCarta();
+cartaElemento.querySelector("#valor").innerHTML = cartaGenerada.valor;
+cartaElemento.querySelector("#palo1").innerHTML = cartaGenerada.palo;
+cartaElemento.querySelector("#palo2").innerHTML = cartaGenerada.palo;
