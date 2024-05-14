dda857997fbca8ef3d9989c7f80251b8f88aaa7e
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-05-14 19:30:27 +0000

    se reformo la idea pricipal y se le agregaron otras funciones

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 function generarCarta() {
-  let palos = [
-    "<i class='fas fa-heart fa-2xs' style='color: #f40101;'></i>",
-    "<i class='fa-solid fa-diamond fa-2xs' style='color: #f50000;'></i>",
-    "&clubs;",
-    "&spades;"
-  ];
+  let palos = ["picas", "trebol", "corazon", "diamante"];
 
   let valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
@@ -15,9 +10,28 @@ function generarCarta() {
   return { valor: valorAleatorio, palo: paloAleatorio };
 }
 
-let cartaElemento = document.getElementById("carta");
+function nuevaCarta() {
+  let cartaGenerada = generarCarta();
+  document.getElementById("valor").innerHTML = cartaGenerada.valor;
+  document.getElementById("palo1").className =
+    "palo divPalo1 " + cartaGenerada.palo;
+  document.getElementById("palo2").className =
+    "palo divPalo2 " + cartaGenerada.palo;
+}
 
-let cartaGenerada = generarCarta();
-cartaElemento.querySelector("#valor").innerHTML = cartaGenerada.valor;
-cartaElemento.querySelector("#palo1").innerHTML = cartaGenerada.palo;
-cartaElemento.querySelector("#palo2").innerHTML = cartaGenerada.palo;
+function dimensiones() {
+  let ancho = document.getElementById("sliderAncho").value;
+  let alto = document.getElementById("sliderAlto").value;
+  document.getElementById("carta").style.width = ancho + "px";
+  document.getElementById("carta").style.height = alto + "px";
+}
+
+const botonCarta = document.getElementById("cambiarCarta");
+botonCarta.addEventListener("click", nuevaCarta);
+
+document.getElementById("sliderAncho").addEventListener("input", dimensiones);
+document.getElementById("sliderAlto").addEventListener("input", dimensiones);
+
+setInterval(nuevaCarta, 10000);
+
+nuevaCarta();
