ecc3204ee3b423bba96749efad67345b71b3d491
Douglas Melendez <160060653+MrDoux@users.noreply.github.com>
2024-05-14 19:35:17 +0000

    se cambia nombre de variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 function generarCarta() {
-  let palos = ["picas", "trebol", "corazon", "diamante"];
+  let palos = ["spade", "club", "heart", "diamond"];
 
   let valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
